6ea5afdcc7f3b07d3151d5669f048fb9f79c3d96
Kieren Holt <kierenholt@gmail.com>
2025-05-12 13:00:22 +0100

    add chromium path to env

--- scraper/Puppeteer.cs
+++ scraper/Puppeteer.cs
@@ -18,7 +18,7 @@ public class PuppeteerBrowser : IAsyncDisposable
         _browser = await extra.LaunchAsync(new LaunchOptions()
         {
             Headless = false,
-            ExecutablePath = "/usr/bin/chromium-browser"
+            ExecutablePath = Environment.GetEnvironmentVariable("CHROMIUM")
         });
 
         _page = await _browser.NewPageAsync();
@@ -35,17 +35,17 @@ public class PuppeteerBrowser : IAsyncDisposable
 
     public async Task AcceptGoogleAcceptPageAsync()
     {
-        await _page.GoToAsync("https://www.google.com", waitUntil: WaitUntilNavigation.Load);
-        await Task.Delay(3000 + new Random().Next(2000));
+        await _page!.GoToAsync("https://www.google.co.uk", waitUntil: WaitUntilNavigation.Load);
+        await Task.Delay(1000 + new Random().Next(1000));
         await _page.EvaluateExpressionAsync("Array.from(document.querySelectorAll(\"button\")).filter(e => e.textContent == \"Accept all\")[0]?.click()");
     }
 
     public async Task<string[]> GetSearchResultsAsync(string needle, int limit = 100)
     {
-        await _page.GoToAsync($"https://www.google.co.uk/search?q={needle}&num={limit}", waitUntil: WaitUntilNavigation.Load);
-        await Task.Delay(1000 + new Random().Next(2000));
+        await _page!.GoToAsync($"https://www.google.co.uk/search?q={needle}&num={limit}", waitUntil: WaitUntilNavigation.Load);
+        await Task.Delay(1000 + new Random().Next(1000));
         await _page.EvaluateExpressionAsync("Array.from(document.querySelectorAll(\"input[value='Accept all']\"))[1]?.click()");
-        await Task.Delay(1000 + new Random().Next(2000));
+        await Task.Delay(1000 + new Random().Next(1000));
         var command = """
         var css = Array.from(document.styleSheets).filter(s => s.ownerNode.parentElement.tagName == "BODY")[0];
         var className = Array.from(css.cssRules).filter(r => r.cssText.indexOf("color: rgb(13, 101, 45)") != -1)[0].selectorText;
@@ -57,9 +57,12 @@ public class PuppeteerBrowser : IAsyncDisposable
 
     public async ValueTask DisposeAsync()
     {
-        await _browser.CloseAsync();
-        await _browser.DisposeAsync();
-        _browser = null;
+        if (_browser != null)
+        {
+            await _browser!.CloseAsync();
+            await _browser.DisposeAsync();
+            _browser = null;
+        }
         _page = null;
     }
 }
